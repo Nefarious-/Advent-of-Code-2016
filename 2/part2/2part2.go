8ede3a80a9de84bdb95e6154e4c76f225256548d
nef <ewan.cook@live.com>
2016-12-03 20:52:31 +0000

    updated day 2

--- 2/part2/2part2.go
+++ 2/part2/2part2.go
@@ -17,33 +17,21 @@ func (k *Keypad) Move(move string) {
 	case "U":
 		if k.Layout[k.Coords[0]-1][k.Coords[1]] != "0" {
 			(*k).Coords[0] -= 1
-			(*k).LastButton = k.Layout[k.Coords[0]][k.Coords[1]]
 		}
 	case "D":
 		if k.Layout[k.Coords[0]+1][k.Coords[1]] != "0" {
 			(*k).Coords[0] += 1
-			(*k).LastButton = k.Layout[k.Coords[0]][k.Coords[1]]
 		}
 	case "L":
 		if k.Layout[k.Coords[0]][k.Coords[1]-1] != "0" {
 			(*k).Coords[1] -= 1
-			(*k).LastButton = k.Layout[k.Coords[0]][k.Coords[1]]
 		}
 	case "R":
 		if k.Layout[k.Coords[0]][k.Coords[1]+1] != "0" {
 			(*k).Coords[1] += 1
-			(*k).LastButton = k.Layout[k.Coords[0]][k.Coords[1]]
 		}
 	}
-}
-
-func (k *Keypad) parseInput(input []string) {
-	for _, y := range input {
-		for i := 0; i < len(y); i++ {
-			k.Move(string(y[i]))
-		}
-		(*k).Buttons = append(k.Buttons, k.LastButton)
-	}
+	(*k).LastButton = k.Layout[k.Coords[0]][k.Coords[1]]
 }
 
 func main() {
@@ -55,9 +43,12 @@ func main() {
 		[]string{"0", "0", "A", "B", "C", "0", "0"},
 		[]string{"0", "0", "0", "D", "0", "0", "0"},
 		[]string{"0", "0", "0", "0", "0", "0", "0"}},
-		[]int{3, 1},
-		make([]string, 0),
-		"5"}
-	k.parseInput(args)
+		[]int{3, 1}, make([]string, 0), "5"}
+	for _, y := range args {
+		for i := 0; i < len(y); i++ {
+			k.Move(string(y[i]))
+		}
+		(&k).Buttons = append(k.Buttons, k.LastButton)
+	}
 	fmt.Println(k.Buttons)
 }

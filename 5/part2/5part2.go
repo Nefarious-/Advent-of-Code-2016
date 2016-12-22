5689369fd52cb180e7553f95022bc201d53335fb
nef <ewan.cook@live.com>
2016-12-22 14:58:33 +0000

    shaved off a few seconds

--- 5/part2/5part2.go
+++ 5/part2/5part2.go
@@ -4,35 +4,32 @@ import (
 	"crypto/md5"
 	"encoding/hex"
 	"fmt"
+	"hash"
 	"os"
 	"strconv"
-	"strings"
 )
 
-func testHash(s string) (bool, byte, int) {
-	h := md5.New()
-	h.Write([]byte(s))
-	res := hex.EncodeToString(h.Sum(nil))
-	pos, err := strconv.ParseInt(string(res[5]), 10, 64)
-	if err != nil {
-		return false, 0, 0
+func testHash(s string, h *hash.Hash) (bool, byte, int64) {
+	(*h).Write([]byte(s))
+	res := hex.EncodeToString((*h).Sum(nil))
+	(*h).Reset()
+	if pos, err := strconv.ParseInt(string(res[5]), 10, 64); err == nil {
+		return res[:5] == "00000", res[6], pos
 	}
-	return strings.HasPrefix(res, "00000"), res[6], int(pos)
+	return false, 0, 0
 }
 
 func main() {
 	input := os.Args[1]
 	pass := make([]byte, 8)
-	var counter int
-	for i := 0; counter < 8; i++ {
-		if ok, v, pos := testHash(fmt.Sprintf("%s%d", input, i)); ok {
-			if pos < 8 && pos >= 0 {
-				if pass[pos] == 0 {
-					pass[pos] = v
-					counter++
-					fmt.Println(string(pass))
-				}
+	h := md5.New()
+	for i, counter := 0, 0; counter < 8; i++ {
+		if ok, v, pos := testHash(fmt.Sprintf("%s%d", input, i), &h); ok {
+			if pos < 8 && pass[pos] == 0 {
+				pass[pos] = v
+				counter++
 			}
 		}
 	}
+	fmt.Println(string(pass))
 }
